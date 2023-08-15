3fde70e14d356742280c46629e6375ebe83171df
00a1 <50469859+00a1@users.noreply.github.com>
2023-08-14 23:32:33 -0500

    1-dimensional tensor?

--- osumapper_v7.0_reproduce_pytorch/maper/losses_torch.py
+++ osumapper_v7.0_reproduce_pytorch/maper/losses_torch.py
@@ -8,10 +8,16 @@ import torch.nn as nn
 #     wall_var_r = torch.where(vg > 1 - border, (vg - (1 - value))**2, torch.zeros_like(vg))
 #     return torch.mean(torch.mean(wall_var_l + wall_var_r, dim=2), dim=1)
 
+# def inblock_loss(vg, border, value):
+#     wall_var_l = torch.where(vg < border, (value - vg)**2, torch.zeros_like(vg))
+#     wall_var_r = torch.where(vg > 1 - border, (vg - (1 - value))**2, torch.zeros_like(vg))
+#     return torch.mean(torch.mean(wall_var_l + wall_var_r, dim=1), dim=1)  # Reduce along dimension 1
+
 def inblock_loss(vg, border, value):
     wall_var_l = torch.where(vg < border, (value - vg)**2, torch.zeros_like(vg))
     wall_var_r = torch.where(vg > 1 - border, (vg - (1 - value))**2, torch.zeros_like(vg))
-    return torch.mean(torch.mean(wall_var_l + wall_var_r, dim=1), dim=1)  # Reduce along dimension 1
+    return torch.mean(wall_var_l + wall_var_r)
+
 
 class GenerativeCustomLoss(nn.Module):
     def __init__(self):
