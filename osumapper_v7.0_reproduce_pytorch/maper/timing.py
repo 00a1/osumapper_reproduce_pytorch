200b682a07e539fadb09da073a74e5225e59d642
00a1 <50469859+00a1@users.noreply.github.com>
2023-08-25 23:39:44 -0500

    dir try 2

--- osumapper_v7.0_reproduce_pytorch/maper/timing.py
+++ osumapper_v7.0_reproduce_pytorch/maper/timing.py
@@ -4,18 +4,18 @@ import numpy as np
 from os_tools import run_command
 import re, os
 
-workingdir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(workingdir)
+file_dir = os.path.dirname(os.path.abspath("TimingAnlyz.exe"))
+exe_file = os.path.join(file_dir, "TimingAnlyz.exe")
 
 def get_timing(music_path):
     """
     Obtain timing by running TimingAnlyz.exe
     """
-    result = run_command(["TimingAnlyz.exe", music_path, "0"]).decode("utf-8")
+    result = run_command([exe_file, music_path, "0"]).decode("utf-8")
     bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
     ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
     if np.abs(bpm - np.round(bpm)) < 0.05:
-        result = run_command(["TimingAnlyz.exe", music_path, str(np.round(bpm))]).decode("utf-8")
+        result = run_command([exe_file, music_path, str(np.round(bpm))]).decode("utf-8")
         bpm = float(re.findall("BPM:\W*([0-9\.]+)", result)[0])
         ofs = float(re.findall("Offset:\W*([0-9\.]+)", result)[0])
     return bpm, ofs
