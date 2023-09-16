92ff0d31e3c73033a5a98225b975d723e3482bcc
00a1 <50469859+00a1@users.noreply.github.com>
2023-09-16 01:23:17 -0500

    check if g loss goes under 0.5

--- osumapper_v7.0_reproduce_pytorch/maper/act_gan_torch_clean.py
+++ osumapper_v7.0_reproduce_pytorch/maper/act_gan_torch_clean.py
@@ -511,7 +511,7 @@ def generate_set_pytorch(models, begin = 0, start_pos=[256, 192], group_id=-1, l
             loss2 = BoxCustomLoss(GAN_PARAMS["box_loss_border"], GAN_PARAMS["box_loss_value"], output[1]) * GAN_PARAMS["box_loss_weight"] # loss2 = g_loss2(output, glabel[1]) * GAN_PARAMS["box_loss_weight"]
             loss3 = GenerativeCustomLoss(output[2]) * 1 # loss3 = g_loss3(output, glabel[2]) * 1
             g_loss = loss1+loss2+loss3
-            # print(g_loss)
+            print(g_loss)
             # g_loss = criterion(output[0], glabel[0]) + criterion(output[1], glabel[1]) + criterion(output[2], glabel[2])
             # g_loss = criterion(output, glabel)
             g_loss.backward()
@@ -546,7 +546,7 @@ def generate_set_pytorch(models, begin = 0, start_pos=[256, 192], group_id=-1, l
             output2 = discriminator(actual_train_data)
             c_loss = criterion(output2, actual_train_labels)
             c_loss.backward(retain_graph=True)
-            print("closs " + str(c_loss))
+            # print("closs " + str(c_loss))
             optimizer_c.step()
 
         if GAN_PARAMS["verbose"]:
